205890b41754e7cdc0d2ab3aa5ebb127ac03b712
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-18 16:55:40 +0200

    prezzo senza sconto

--- js/main.js
+++ js/main.js
@@ -4,36 +4,48 @@ const età = document.getElementById("age-input");
 
 const Button = document.getElementById("calc-price");
 
-const prezzo = kiloMetri.value * 0.21;
+
 
 Button.addEventListener("click" , function(){
+
+   const prezzo = kiloMetri.value * 0.21;
 
     console.log(kiloMetri.value);
 
     console.log(età.value);
 
     console.log(prezzo);
+
+
+    let prezzoScontato = prezzo
+
+    if(età.value<18) {
+    
+        let prezzoScontato = prezzo * 0.80;
+    
+    
+    } else if(età.value>65) {
+    
+        let prezzoScontato = prezzo * 0.60;
+    
+    } else {
+    
+     let prezzoScontato = prezzo
+    
+    }
+
+    console.log(prezzoScontato);
+
+
+
+
+
 })
 
 
 
 
-let prezzoScontato = prezzo
 
-if(età.value<18) {
-
-    let prezzoScontato = prezzo * 0.80;
-
-
-} else if(età.value>65) {
-
-    let prezzoScontato = prezzo * 0.60;
-
-} else {
-
- let prezzoScontato = prezzo
-
-}
 
 
 
